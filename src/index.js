08b56e6418b43c3f053167d5250286609a8c85ee
Koenraad Verheyden <github@kvrhdn.dev>
2020-04-29 23:50:27 +0200

    Refactor internals; add steps to init & post

--- src/index.js
+++ src/index.js
@@ -1,64 +1,30 @@
-const fs = require('fs');
 const core = require('@actions/core');
-const exec = require('@actions/exec');
-const logfmt = require('logfmt');
+const buildevents = require('./buildevents.js');
 const util = require('./util.js');
 
 async function run() {
   try {
     const buildStart = util.getTimestamp();
+    const buildId = util.getEnv('GITHUB_RUN_NUMBER');
+
+    // save buildStart to be used in the post section
     core.saveState('buildStart', buildStart.toString());
+    // set BUILD_ID to be used throughout the workflow
+    util.setEnv('BUILD_ID', buildId);
 
     const apikey = core.getInput('apikey', { required: true })
     core.setSecret(apikey);
     const dataset = core.getInput('dataset', { required: true });
 
-    await installBuildevents();
+    await buildevents.install(apikey, dataset);
 
-    configureBuildevents(apikey, dataset);
-
-    util.setEnv('BUILD_ID', util.getEnv('GITHUB_RUN_NUMBER'));
+    await buildevents.step(buildId, 1000, buildStart, 'init');
 
     console.log('Init done! buildevents is now available on the path.');
 
   } catch (error) {
     core.setFailed(error.message);
   }
-}
-
-async function installBuildevents() {
-  console.log('Downloading buildevents to /usr/local/bin/buildevents');
-  let ret = await exec.exec('sudo curl -L -o /usr/local/bin/buildevents https://github.com/honeycombio/buildevents/releases/latest/download/buildevents-linux-amd64');
-  if (ret != 0) {
-    throw new Error('Downloading buildevents failed');
-  }
-
-  console.log('Setting permissions for using buildevents');
-  ret = await exec.exec('sudo chmod 755 /usr/local/bin/buildevents');
-  if (ret != 0) {
-    throw new Error('Making buildevents exectable failed');
-  }
-}
-
-function configureBuildevents(apikey, dataset) {
-  // environment variables used by buildevents
-  util.setEnv('BUILDEVENT_APIKEY', apikey);
-  util.setEnv('BUILDEVENT_DATASET', dataset);
-  util.setEnv('BUILDEVENT_CIPROVIDER', 'github-actions');
-
-  const variables = logfmt.stringify({
-    'github.workflow': util.getEnv('GITHUB_WORKFLOW'),
-    'github.run_id': util.getEnv('GITHUB_RUN_ID'),
-    'github.run_number': util.getEnv('GITHUB_RUN_NUMBER'),
-    'github.actor': util.getEnv('GITHUB_ACTOR'),
-    'github.repository': util.getEnv('GITHUB_REPOSITORY'),
-    'github.event_name': util.getEnv('GITHUB_EVENT_NAME'),
-    'github.sha': util.getEnv('GITHUB_SHA'),
-    'github.ref': util.getEnv('GITHUB_REF'),
-  });
-  fs.writeFileSync('../buildevents.txt', variables);
-
-  util.setEnv('BUILDEVENT_FILE', '../buildevents.txt');
 }
 
 run();
--- src/post.js
+++ src/post.js
@@ -1,19 +1,31 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
+const buildevents = require('./buildevents.js');
 const util = require('./util.js');
 
 async function runPost() {
   try {
+    const postStart = util.getTimestamp();
+
     const buildId = util.getEnv('BUILD_ID');
-    const buildStart = core.getState('buildStart')
+    const buildStart = core.getState('buildStart');
 
     const jobStatus = core.getInput('job-status', { required: true });
     const result = jobStatus == 'Success' ? 'success' : 'failure';
 
-    let ret = await exec.exec(`buildevents build ${buildId} ${buildStart} ${result}`);
-    if (ret != 0) {
-      throw new Error('Could not build trace');
-    }
+    buildevents.addFields({
+      'github.workflow': util.getEnv('GITHUB_WORKFLOW'),
+      'github.run_id': util.getEnv('GITHUB_RUN_ID'),
+      'github.run_number': util.getEnv('GITHUB_RUN_NUMBER'),
+      'github.actor': util.getEnv('GITHUB_ACTOR'),
+      'github.repository': util.getEnv('GITHUB_REPOSITORY'),
+      'github.event_name': util.getEnv('GITHUB_EVENT_NAME'),
+      'github.sha': util.getEnv('GITHUB_SHA'),
+      'github.ref': util.getEnv('GITHUB_REF'),
+      'job.status': jobStatus,
+    });
+
+    await buildevents.step(buildId, 1001, postStart, 'post');
+    await buildevents.build(buildId, buildStart, result);
 
   } catch (error) {
     core.setFailed(error.message);

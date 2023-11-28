15c0adcc0c4b90c18f5874d1e90a3532f13f7a28
brookesargent <brookesargent@honeycomb.io>
2023-11-28 14:40:27 -0500

    npm run all

--- dist/index.js
+++ dist/index.js
@@ -48,7 +48,7 @@ const io = __importStar(__nccwpck_require__(7436));
 const tc = __importStar(__nccwpck_require__(7784));
 const logfmt = __importStar(__nccwpck_require__(5578));
 const util = __importStar(__nccwpck_require__(4024));
-function install(apikey, apiHost, dataset) {
+function install(apikey, apihost, dataset) {
     return __awaiter(this, void 0, void 0, function* () {
         core.info('Downloading and installing buildevents');
         const url = 'https://github.com/honeycombio/buildevents/releases/latest/download/' + util.constructExecutableName();
@@ -61,7 +61,7 @@ function install(apikey, apiHost, dataset) {
         yield exec.exec(`chmod +x ${toolPath}`);
         core.addPath(path.dirname(toolPath));
         util.setEnv('BUILDEVENT_APIKEY', apikey);
-        util.setEnv('BUILDEVENT_APIHOST', apiHost);
+        util.setEnv('BUILDEVENT_APIHOST', apihost);
         util.setEnv('BUILDEVENT_DATASET', dataset);
         util.setEnv('BUILDEVENT_CIPROVIDER', 'gha-buildevents');
     });
@@ -171,9 +171,9 @@ function run() {
             const apikey = core.getInput('apikey', { required: true });
             core.setSecret(apikey);
             // defaults to api.honeycomb.io
-            const apiHost = core.getInput('apiHost') !== '' ? core.getInput('apiHost') : 'https://api.honeycomb.io';
+            const apihost = core.getInput('apihost') !== '' ? core.getInput('apihost') : 'https://api.honeycomb.io';
             const dataset = core.getInput('dataset', { required: true });
-            yield buildevents.install(apikey, apiHost, dataset);
+            yield buildevents.install(apikey, apihost, dataset);
             buildevents.addFields({
                 // available environment variables
                 // https://docs.github.com/en/actions/configuring-and-managing-workflows/using-environment-variables#default-environment-variables

803d2b6497534c82a554265a02ea2f7bf7375061
Jason Harley <jason@honeycomb.io>
2024-02-02 11:42:25 -0500

    fix: npm run pack

--- dist/index.js
+++ dist/index.js
@@ -182,15 +182,15 @@ function run() {
                 'github.run_number': util.getEnv('GITHUB_RUN_NUMBER'),
                 'github.actor': util.getEnv('GITHUB_ACTOR'),
                 'github.repository': util.getEnv('GITHUB_REPOSITORY'),
-                'github.repository_owner': util.getEnv('GITHUB_REPOSITORY_OWNER'), // undocumented
+                'github.repository_owner': util.getEnv('GITHUB_REPOSITORY_OWNER'),
                 'github.event_name': util.getEnv('GITHUB_EVENT_NAME'),
                 'github.sha': util.getEnv('GITHUB_SHA'),
                 'github.ref': util.getEnv('GITHUB_REF'),
                 'github.head_ref': util.getEnv('GITHUB_HEAD_REF'),
                 'github.base_ref': util.getEnv('GITHUB_BASE_REF'),
-                'github.job': util.getEnv('GITHUB_JOB'), // undocumented
+                'github.job': util.getEnv('GITHUB_JOB'),
                 'github.matrix-key': core.getInput('matrix-key'),
-                'runner.os': util.getEnv('RUNNER_OS'), // undocumented
+                'runner.os': util.getEnv('RUNNER_OS'),
                 'meta.source': 'gha-buildevents'
             });
             // create a first step to time installation of buildevents
@@ -899,7 +899,7 @@ class OidcClient {
                 .catch(error => {
                 throw new Error(`Failed to get ID Token. \n 
         Error Code : ${error.statusCode}\n 
-        Error Message: ${error.message}`);
+        Error Message: ${error.result.message}`);
             });
             const id_token = (_a = res.result) === null || _a === void 0 ? void 0 : _a.value;
             if (!id_token) {
